87448789588438e8b6b1264b438586b4ff3821f2
cyco130 <cyco130@gmail.com>
2022-06-03 14:22:10 +0300

    feat: upgrade to vite 3

---
+++ server.js
@@ -0,0 +1,93 @@
+// @ts-check
+import fs from "fs";
+import path from "path";
+import express from "express";
+
+const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
+
+process.env.MY_CUSTOM_SECRET = "API_KEY_qwertyuiop";
+
+async function createServer(
+	root = process.cwd(),
+	isProd = process.env.NODE_ENV === "production",
+) {
+	const indexProd = isProd
+		? fs.readFileSync("./dist/client/index.html", "utf-8")
+		: "";
+
+	const app = express();
+
+	/**
+	 * @type {import('vite').ViteDevServer}
+	 */
+	let vite;
+	if (!isProd) {
+		vite = await (
+			await import("vite")
+		).createServer({
+			root,
+			logLevel: isTest ? "error" : "info",
+			server: {
+				middlewareMode: "ssr",
+				watch: {
+					// During tests we edit the files too fast and sometimes chokidar
+					// misses change events, so enforce polling for consistency
+					usePolling: true,
+					interval: 100,
+				},
+			},
+		});
+		// use vite's connect instance as middleware
+		app.use(vite.middlewares);
+	} else {
+		const compression = await import("compression");
+		const serveStatic = await import("serve-static");
+		app.use(compression.default());
+		app.use(
+			serveStatic.default("./dist/client", {
+				index: false,
+			}),
+		);
+	}
+
+	app.use("*", async (req, res) => {
+		try {
+			const url = req.originalUrl;
+
+			let template, render;
+			if (!isProd) {
+				// always read fresh template in dev
+				template = fs.readFileSync("./index.html", "utf-8");
+				template = await vite.transformIndexHtml(url, template);
+				render = (await vite.ssrLoadModule("/src/entry-server.jsx")).render;
+			} else {
+				template = indexProd;
+				render = (await import("./dist/server/entry-server.js")).render;
+			}
+
+			const context = {};
+			const appHtml = render(url, context);
+
+			if (context.url) {
+				// Somewhere a `<Redirect>` was rendered
+				return res.redirect(301, context.url);
+			}
+
+			const html = template.replace(`<!--app-html-->`, appHtml);
+
+			res.status(200).set({ "Content-Type": "text/html" }).end(html);
+		} catch (e) {
+			!isProd && vite.ssrFixStacktrace(e);
+			console.log(e.stack);
+			res.status(500).end(e.stack);
+		}
+	});
+
+	return { app, vite };
+}
+
+createServer().then(({ app }) =>
+	app.listen(3000, () => {
+		console.log("http://localhost:3000");
+	}),
+);
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react";
+
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default {
+	plugins: [react()],
+	build: {
+		minify: false,
+	},
+};
